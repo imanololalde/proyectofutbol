32aa2cf3a798e95ea10e5aaaf7140c860073a8d6
imanololalde <imanololalde@E110L22.opendeusto.es>
2018-11-09 13:50:47 +0100

    Fotos mas trabajo en la ventana de partidos #avanzando

--- proyectofutbol/src/ventanas/VentanaPartidos.java
+++ proyectofutbol/src/ventanas/VentanaPartidos.java
@@ -8,8 +8,13 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Color;
+import java.awt.Font;
 
 public class VentanaPartidos extends JFrame  {
 
@@ -20,7 +25,7 @@ public class VentanaPartidos extends JFrame  {
 	
 	private JPanel contentPane;
 	public String[] formacion={"2-3-1", "2-2-2", "3-2-1", "3-1-2"};
-	public JComboBox jugador1;
+	public Object seleccionPor, seleccionDef1, seleccionDef2, seleccionCen1, seleccionCen2, seleccionCen3,seleccionDel;
 
 	/**
 	 * Launch the application.
@@ -58,41 +63,115 @@ public class VentanaPartidos extends JFrame  {
 			}
 		});
 		
-		JComboBox comboBox_6 = new JComboBox();
-		comboBox_6.setBounds(301, 53, 71, 20);
-		contentPane.add(comboBox_6);
+		JLabel nomdef2 = new JLabel("");
+		nomdef2.setForeground(Color.WHITE);
+		nomdef2.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nomdef2.setBounds(412, 222, 89, 29);
+		contentPane.add(nomdef2);
 		
-		JComboBox jugador6 = new JComboBox();
-		jugador6.setBounds(395, 159, 71, 20);
-		contentPane.add(jugador6);
 		
-		JComboBox jugador5 = new JComboBox();
-		jugador5.setBounds(177, 159, 71, 20);
-		contentPane.add(jugador5);
 		
-		JComboBox jugador3 = new JComboBox();
-		jugador3.setBounds(107, 349, 71, 20);
-		contentPane.add(jugador3);
+		JLabel Delantero1 = new JLabel("");
+		Delantero1.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/DEL.png")));
+		Delantero1.setBounds(307, 410, 65, 65);
+		contentPane.add(Delantero1);
 		
-		JComboBox jugador2 = new JComboBox();
-		jugador2.setBounds(472, 349, 71, 20);
-		contentPane.add(jugador2);
+		JLabel nombreportero = new JLabel();
+		nombreportero.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nombreportero.setForeground(Color.WHITE);
+		nombreportero.setBounds(299, 109, 89, 29);
+		contentPane.add(nombreportero);
 		
-		JComboBox jugador4 = new JComboBox();
-		jugador4.setBounds(301, 269, 71, 20);
-		contentPane.add(jugador4);
+		JLabel nomdef1 = new JLabel("");
+		nomdef1.setForeground(Color.WHITE);
+		nomdef1.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nomdef1.setBounds(150, 220, 89, 29);
+		contentPane.add(nomdef1);
+		
+		JLabel nomcen1 = new JLabel("");
+		nomcen1.setForeground(Color.WHITE);
+		nomcen1.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nomcen1.setBounds(299, 323, 89, 29);
+		contentPane.add(nomcen1);
+		
+		JLabel nomcen2 = new JLabel("");
+		nomcen2.setForeground(Color.WHITE);
+		nomcen2.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nomcen2.setBounds(88, 406, 89, 29);
+		contentPane.add(nomcen2);
+		
+		JLabel nomcen3 = new JLabel("");
+		nomcen3.setForeground(Color.WHITE);
+		nomcen3.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nomcen3.setBounds(496, 406, 89, 29);
+		contentPane.add(nomcen3);
+		
+		JLabel nomdel = new JLabel("");
+		nomdel.setForeground(Color.WHITE);
+		nomdel.setFont(new Font("SpaceClaim ASME CB", Font.BOLD, 11));
+		nomdel.setBounds(382, 443, 89, 29);
+		contentPane.add(nomdel);
+		
+		JLabel Centro3 = new JLabel("");
+		Centro3.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/MED.png")));
+		Centro3.setBounds(509, 330, 65, 65);
+		contentPane.add(Centro3);
+		
+		JLabel Defensa2 = new JLabel("");
+		Defensa2.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/DEF.png")));
+		Defensa2.setBounds(424, 146, 65, 65);
+		contentPane.add(Defensa2);
+		
+		JLabel Portero = new JLabel("");
+		Portero.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				 seleccionPor = JOptionPane.showInputDialog( null,
+						   "Seleccione Portero",
+						   "Selector de portero",
+						   JOptionPane.QUESTION_MESSAGE,
+						   null,
+						   new Object[] { "Javi", "Carlos", "Eleder" }, 
+						   "Portero");
+				 
+				 nombreportero.setText((String) seleccionPor);
+				 
+			}
+		});
+		Portero.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/POR.png")));
+		Portero.setBounds(307, 36, 65, 65);
+		contentPane.add(Portero);
+		
+		JLabel Defensa1 = new JLabel("");
+		Defensa1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				seleccionDef1 = JOptionPane.showInputDialog( null,
+						   "Seleccione Defensa Central",
+						   "Selector de Defensa Central",
+						   JOptionPane.QUESTION_MESSAGE,
+						   null,
+						   new Object[] { "Xabi", "Aitor 'EL COCHES'", "Lucas" }, 
+						   "Defensa Central");
+				 
+				 nomdef1.setText((String) seleccionDef1);
+			}
+		});
+		Defensa1.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/DEF.png")));
+		Defensa1.setBounds(164, 146, 65, 65);
+		contentPane.add(Defensa1);
+		
+		JLabel Centro2 = new JLabel("");
+		Centro2.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/MED.png")));
+		Centro2.setBounds(100, 330, 65, 65);
+		contentPane.add(Centro2);
+		
+		JLabel Centro1 = new JLabel("");
+		Centro1.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/MED.png")));
+		Centro1.setBounds(307, 247, 65, 65);
+		contentPane.add(Centro1);
 		btnAtras.setBounds(585, 527, 89, 23);
 		contentPane.add(btnAtras);
-		
-		jugador1 = new JComboBox();
-		jugador1.setBounds(301, 464, 71, 20);
-		contentPane.add(jugador1);
-		jugador1.addActionListener(jugador1);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\imanololalde\\Desktop\\proyectofutbolgit\\fotillos\\campo bueno.jpg"));
-		lblNewLabel.setBounds(10, 11, 664, 505);
-		contentPane.add(lblNewLabel);
 		
 		JButton btnGuardarCambios = new JButton("Guardar cambios");
 		btnGuardarCambios.setBounds(10, 527, 136, 23);
@@ -101,8 +180,17 @@ public class VentanaPartidos extends JFrame  {
 		JComboBox formaciones = new JComboBox(formacion);
 		formaciones.setBounds(299, 528, 73, 20);
 		contentPane.add(formaciones);
+		
+		
+		
+		JLabel Campo = new JLabel("");
+		Campo.setIcon(new ImageIcon(VentanaPartidos.class.getResource("/imagenes/campo bueno.jpg")));
+		Campo.setBounds(10, 0, 664, 505);
+		contentPane.add(Campo);
+		
+		
 		Object eleccion= formaciones.getSelectedItem();
+		
 
         }
-		  
 	}
